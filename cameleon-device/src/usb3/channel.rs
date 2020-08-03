25cba6f79c0f89f1c030863259b8cd666d81ff07
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2020-08-04 01:10:10 +0900

    Implement ReceiveChannel

--- cameleon-device/src/usb3/channel.rs
+++ cameleon-device/src/usb3/channel.rs
@@ -5,7 +5,6 @@ use super::Result;
 
 pub struct ControlChannel {
     device_handle: RusbDevHandle,
-
     iface_info: ControlIfaceInfo,
 }
 
@@ -59,9 +58,54 @@ impl Drop for ControlChannel {
     }
 }
 
+pub struct ReceiveChannel {
+    device_handle: RusbDevHandle,
+    iface_info: ReceiveIfaceInfo,
+}
+
+impl ReceiveChannel {
+    pub(super) fn new(device_handle: RusbDevHandle, iface_info: ReceiveIfaceInfo) -> Self {
+        Self {
+            device_handle,
+            iface_info,
+        }
+    }
+
+    pub fn open(&mut self) -> Result<()> {
+        self.device_handle
+            .claim_interface(self.iface_info.iface_number)?;
+
+        Ok(())
+    }
+
+    pub fn close(&mut self) -> Result<()> {
+        self.device_handle
+            .release_interface(self.iface_info.iface_number)?;
+
+        Ok(())
+    }
+
+    pub fn recv(&self, buf: &mut [u8], timeout: time::Duration) -> Result<usize> {
+        Ok(self
+            .device_handle
+            .read_bulk(self.iface_info.bulk_in_ep, buf, timeout)?)
+    }
+
+    pub fn clear_halt(&mut self) -> Result<()> {
+        self.device_handle.clear_halt(self.iface_info.bulk_in_ep)?;
+        Ok(())
+    }
+}
+
 #[derive(Clone, Debug)]
 pub(super) struct ControlIfaceInfo {
     pub(super) iface_number: u8,
     pub(super) bulk_in_ep: u8,
     pub(super) bulk_out_ep: u8,
+}
+
+#[derive(Clone, Debug)]
+pub(super) struct ReceiveIfaceInfo {
+    pub(super) iface_number: u8,
+    pub(super) bulk_in_ep: u8,
 }
