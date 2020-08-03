ea765943642eb11120bf7dd1c8f71ffd62a0de08
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2020-08-03 22:07:49 +0900

    Implement usb3 device builder

--- cameleon-device/src/usb3/mod.rs
+++ cameleon-device/src/usb3/mod.rs
@@ -3,12 +3,21 @@ use thiserror::Error;
 pub mod control_handle;
 pub mod device;
 
+mod device_builder;
+
 pub use device::Device;
+pub use device_builder::enumerate_device;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error(transparent)]
     LibusbError(#[from] rusb::Error),
+
+    #[error("buffer io error: {}", 0)]
+    BufferIoError(#[from] std::io::Error),
+
+    #[error("device doesn't follow specification")]
+    InvalidDevice,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
