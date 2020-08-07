2458d6e490326e9208c7244257d4070a8719dd9b
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2020-08-07 19:37:40 +0900

    Add access right enum

--- cameleon-device/src/usb3/register_map.rs
+++ cameleon-device/src/usb3/register_map.rs
@@ -1,68 +1,117 @@
-/// (Address, Length) of all entries in Technology Agnostic Boot Register Map (ABRM).
+/// (Address, Length, Access Right) of all entries in Technology Agnostic Boot Register Map (ABRM).
 pub mod abrm {
-    pub const GENCP_VERSION: (u64, u16) = (0x0000, 4);
-    pub const MANUFACTURER_NAME: (u64, u16) = (0x0004, 64);
-    pub const MODEL_NAME: (u64, u16) = (0x0044, 64);
-    pub const FAMILY_NAME: (u64, u16) = (0x0084, 64);
-    pub const DEVICE_VERSION: (u64, u16) = (0x00C4, 64);
-    pub const MANUFACTURER_INFO: (u64, u16) = (0x0104, 64);
-    pub const SERIAL_NUMBER: (u64, u16) = (0x0144, 64);
-    pub const USER_DEFINED_NAME: (u64, u16) = (0x0184, 64);
-    pub const DEVICE_CAPABILITY: (u64, u16) = (0x01C4, 8);
-    pub const MAXIMUM_DEVICE_RESPONSE_TIME: (u64, u16) = (0x01CC, 4);
-    pub const MANIFEST_TABLE_ADDRESS: (u64, u16) = (0x01D0, 8);
-    pub const SBRM_ADDRESS: (u64, u16) = (0x01D8, 8);
-    pub const DEVICE_CONFIGURATION: (u64, u16) = (0x01E0, 8);
-    pub const HEARTBEAT_TIMEOUT: (u64, u16) = (0x01E8, 4);
-    pub const MESSAGE_CHANNEL_ID: (u64, u16) = (0x01EC, 4);
-    pub const TIMESTAMP: (u64, u16) = (0x01F0, 8);
-    pub const TIMESTAMP_LATCH: (u64, u16) = (0x01F8, 4);
-    pub const TIMESTAMP_INCREMENT: (u64, u16) = (0x01FC, 8);
-    pub const ACCESS_PRIVILEGE: (u64, u16) = (0x0204, 4);
-    pub const PROTOCOL_ENDIANESS: (u64, u16) = (0x0208, 4);
-    pub const IMPLEMENTATION_ENDIANESS: (u64, u16) = (0x020C, 4);
-    pub const DEVICE_SOFTWARE_INTERFACE_VERSION: (u64, u16) = (0x0210, 64);
+    use super::AccessRight::{self, *};
+    pub const GENCP_VERSION: (u64, u16, AccessRight) = (0x0000, 4, RO);
+    pub const MANUFACTURER_NAME: (u64, u16, AccessRight) = (0x0004, 64, RO);
+    pub const MODEL_NAME: (u64, u16, AccessRight) = (0x0044, 64, RO);
+    pub const FAMILY_NAME: (u64, u16, AccessRight) = (0x0084, 64, RO);
+    pub const DEVICE_VERSION: (u64, u16, AccessRight) = (0x00C4, 64, RO);
+    pub const MANUFACTURER_INFO: (u64, u16, AccessRight) = (0x0104, 64, RO);
+    pub const SERIAL_NUMBER: (u64, u16, AccessRight) = (0x0144, 64, RO);
+    pub const USER_DEFINED_NAME: (u64, u16, AccessRight) = (0x0184, 64, RW);
+    pub const DEVICE_CAPABILITY: (u64, u16, AccessRight) = (0x01C4, 8, RO);
+    pub const MAXIMUM_DEVICE_RESPONSE_TIME: (u64, u16, AccessRight) = (0x01CC, 4, RO);
+    pub const MANIFEST_TABLE_ADDRESS: (u64, u16, AccessRight) = (0x01D0, 8, RO);
+    pub const SBRM_ADDRESS: (u64, u16, AccessRight) = (0x01D8, 8, RO);
+    pub const DEVICE_CONFIGURATION: (u64, u16, AccessRight) = (0x01E0, 8, RO);
+    pub const HEARTBEAT_TIMEOUT: (u64, u16, AccessRight) = (0x01E8, 4, RW);
+    pub const MESSAGE_CHANNEL_ID: (u64, u16, AccessRight) = (0x01EC, 4, RW);
+    pub const TIMESTAMP: (u64, u16, AccessRight) = (0x01F0, 8, RO);
+    pub const TIMESTAMP_LATCH: (u64, u16, AccessRight) = (0x01F8, 4, WO);
+    pub const TIMESTAMP_INCREMENT: (u64, u16, AccessRight) = (0x01FC, 8, RO);
+    pub const ACCESS_PRIVILEGE: (u64, u16, AccessRight) = (0x0204, 4, RW);
+    pub const PROTOCOL_ENDIANESS: (u64, u16, AccessRight) = (0x0208, 4, RO);
+    pub const IMPLEMENTATION_ENDIANESS: (u64, u16, AccessRight) = (0x020C, 4, RO);
+    pub const DEVICE_SOFTWARE_INTERFACE_VERSION: (u64, u16, AccessRight) = (0x0210, 64, RO);
 }
 
-/// (Offset, Length) of entries in Technology Specific Boot Register Map (SBRM).
+/// (Offset, Length, Access Right) of entries in Technology Specific Boot Register Map (SBRM).
 /// SBRM base address can be obtained by reading [`abrm::SBRM_ADDRESS`](../abrm/constant.SBRM_ADDRESS.html)
 pub mod sbrm {
-    pub const U3V_VERSION: (u64, u16) = (0x0000, 4);
-    pub const U3VCP_CAPABILITY_REGISTER: (u64, u16) = (0x0004, 8);
-    pub const U3VCP_CONFIGURATION_REGISTER: (u64, u16) = (0x000C, 8);
-    pub const MAXIMUM_COMMAND_TRANSFER_LENGTH: (u64, u16) = (0x014, 4);
-    pub const MAXIMUM_ACKNOWLEDGE_TRANSFER_LENGTH: (u64, u16) = (0x018, 4);
-    pub const NUMBER_OF_STREAM_CHANNELS: (u64, u16) = (0x01C, 4);
-    pub const SIRM_ADDRESS: (u64, u16) = (0x020, 8);
-    pub const SIRM_LENGTH: (u64, u16) = (0x028, 4);
-    pub const EIRM_ADDRESS: (u64, u16) = (0x02C, 8);
-    pub const EIRM_LENGTH: (u64, u16) = (0x034, 4);
-    pub const IIDC2_ADDRESS: (u64, u16) = (0x038, 8);
-    pub const CURRENT_SPEED: (u64, u16) = (0x040, 4);
+    use super::AccessRight::{self, *};
+    pub const U3V_VERSION: (u64, u16, AccessRight) = (0x0000, 4, RO);
+    pub const U3VCP_CAPABILITY_REGISTER: (u64, u16, AccessRight) = (0x0004, 8, RO);
+    pub const U3VCP_CONFIGURATION_REGISTER: (u64, u16, AccessRight) = (0x000C, 8, RW);
+    pub const MAXIMUM_COMMAND_TRANSFER_LENGTH: (u64, u16, AccessRight) = (0x014, 4, RO);
+    pub const MAXIMUM_ACKNOWLEDGE_TRANSFER_LENGTH: (u64, u16, AccessRight) = (0x018, 4, RO);
+    pub const NUMBER_OF_STREAM_CHANNELS: (u64, u16, AccessRight) = (0x01C, 4, RO);
+    pub const SIRM_ADDRESS: (u64, u16, AccessRight) = (0x020, 8, RO);
+    pub const SIRM_LENGTH: (u64, u16, AccessRight) = (0x028, 4, RO);
+    pub const EIRM_ADDRESS: (u64, u16, AccessRight) = (0x02C, 8, RO);
+    pub const EIRM_LENGTH: (u64, u16, AccessRight) = (0x034, 4, RO);
+    pub const IIDC2_ADDRESS: (u64, u16, AccessRight) = (0x038, 8, RO);
+    pub const CURRENT_SPEED: (u64, u16, AccessRight) = (0x040, 4, RO);
 }
 
-/// (Offset, Length) of all entries in Event Interface Register Map (EIRM).
+/// (Offset, Length, Access Right) of all entries in Event Interface Register Map (EIRM).
 /// SIRM base address can be obtained by
 /// [`sbrm::EIRM_ADDRESS`](../sirm/constant.EIRM_ADDRESS.html).
 pub mod eirm {
-    pub const EI_CONTROL: (u64, u16) = (0x0000, 4);
-    pub const MAXIMUM_EVENT_TRANSFER_LENGTH: (u64, u16) = (0x0004, 4);
-    pub const EVENT_TEST_CONTROL: (u64, u16) = (0x0008, 4);
+    use super::AccessRight::{self, *};
+    pub const EI_CONTROL: (u64, u16, AccessRight) = (0x0000, 4, RW);
+    pub const MAXIMUM_EVENT_TRANSFER_LENGTH: (u64, u16, AccessRight) = (0x0004, 4, RO);
+    pub const EVENT_TEST_CONTROL: (u64, u16, AccessRight) = (0x0008, 4, RO);
 }
 
-/// (Offset, Length) of all entries in Streaming Interface Register Map (SIRM).
+/// (Offset, Length, AccessRight) of all entries in Streaming Interface Register Map (SIRM).
 /// SIRM base address can be obtained by
 /// [`sbrm::SIRM_ADDRESS`](../sirm/constant.SIRM_ADDRESS.html).
 pub mod sirm {
-    pub const SI_INFO: (u64, u16) = (0x0000, 4);
-    pub const SI_CONTROL: (u64, u16) = (0x0004, 4);
-    pub const REQUIRED_PAYLOAD_SIZE: (u64, u16) = (0x0008, 8);
-    pub const REQUIRED_LEADER_SIZE: (u64, u16) = (0x0010, 4);
-    pub const REQUIRED_TRAILER_SIZE: (u64, u16) = (0x0014, 4);
-    pub const MAXIMUM_LEADER_SIZE: (u64, u16) = (0x0018, 4);
-    pub const PAYLOAD_TRANSFER_SIZE: (u64, u16) = (0x001C, 4);
-    pub const PAYLOAD_TRANSFER_COUNT: (u64, u16) = (0x0020, 4);
-    pub const PAYLOAD_FINAL_TRANSFER1_SIZE: (u64, u16) = (0x0024, 4);
-    pub const PAYLOAD_FINAL_TRANSFER2_SIZE: (u64, u16) = (0x0028, 4);
-    pub const MAXIMUM_TRAILER_SIZE: (u64, u16) = (0x002C, 4);
+    use super::AccessRight::{self, *};
+    pub const SI_INFO: (u64, u16, AccessRight) = (0x0000, 4, RO);
+    pub const SI_CONTROL: (u64, u16, AccessRight) = (0x0004, 4, RW);
+    pub const REQUIRED_PAYLOAD_SIZE: (u64, u16, AccessRight) = (0x0008, 8, RO);
+    pub const REQUIRED_LEADER_SIZE: (u64, u16, AccessRight) = (0x0010, 4, RO);
+    pub const REQUIRED_TRAILER_SIZE: (u64, u16, AccessRight) = (0x0014, 4, RO);
+    pub const MAXIMUM_LEADER_SIZE: (u64, u16, AccessRight) = (0x0018, 4, RW);
+    pub const PAYLOAD_TRANSFER_SIZE: (u64, u16, AccessRight) = (0x001C, 4, RW);
+    pub const PAYLOAD_TRANSFER_COUNT: (u64, u16, AccessRight) = (0x0020, 4, RW);
+    pub const PAYLOAD_FINAL_TRANSFER1_SIZE: (u64, u16, AccessRight) = (0x0024, 4, RW);
+    pub const PAYLOAD_FINAL_TRANSFER2_SIZE: (u64, u16, AccessRight) = (0x0028, 4, RW);
+    pub const MAXIMUM_TRAILER_SIZE: (u64, u16, AccessRight) = (0x002C, 4, RW);
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum AccessRight {
+    /// Not Available.
+    NA,
+
+    /// ReadOnly.
+    RO,
+
+    /// WriteOnly.
+    WO,
+
+    /// ReadWrite.
+    RW,
+}
+
+impl AccessRight {
+    pub fn is_readable(self) -> bool {
+        self.as_num() & 0b1 == 1
+    }
+
+    pub fn is_writeable(self) -> bool {
+        self.as_num() >> 1 == 1
+    }
+
+    pub(crate) fn as_num(self) -> u8 {
+        match self {
+            Self::NA => 0b00,
+            Self::RO => 0b01,
+            Self::WO => 0b10,
+            Self::RW => 0b11,
+        }
+    }
+
+    pub(crate) fn from_num(num: u8) -> Self {
+        debug_assert!(num >> 2 == 0);
+        match num {
+            0b00 => Self::NA,
+            0b01 => Self::RO,
+            0b10 => Self::WO,
+            0b11 => Self::RW,
+            _ => unreachable!(),
+        }
+    }
 }
