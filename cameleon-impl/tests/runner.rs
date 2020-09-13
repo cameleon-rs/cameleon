34649420c4abe146b37598be45137382ac891080
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2020-09-14 01:16:09 +0900

    Change tests directory structure

--- cameleon-impl/tests/runner.rs
+++ cameleon-impl/tests/runner.rs
@@ -1,11 +1,11 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/register.rs");
-    t.compile_fail("tests/wrong_endianess.rs");
-    t.compile_fail("tests/wrong_access_right.rs");
-    t.compile_fail("tests/wrong_ty_attr1.rs");
-    t.compile_fail("tests/wrong_ty_attr2.rs");
+    t.pass("tests/macros/register.rs");
+    t.pass("tests/macros/memory.rs");
 
-    t.pass("tests/memory.rs");
+    t.compile_fail("tests/macros/wrong_endianess.rs");
+    t.compile_fail("tests/macros/wrong_access_right.rs");
+    t.compile_fail("tests/macros/wrong_ty_attr1.rs");
+    t.compile_fail("tests/macros/wrong_ty_attr2.rs");
 }
