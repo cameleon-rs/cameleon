0cd8662cc2a1f2e85bb3e5ae29ce6e3b7dd9fe82
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2020-11-04 23:33:28 +0900

    Define Interface related partially

--- cameleon-gentl/src/interface/mod.rs
+++ cameleon-gentl/src/interface/mod.rs
@@ -1,1 +1,16 @@
+pub mod u3v;
+
 mod u3v_memory;
+
+// TODO: Add device related functions.
+pub trait Interface {
+    fn unique_id(&self) -> &'static str;
+
+    fn mac_addr(&self) -> Option<[u8; 6]>;
+
+    fn ip_addr(&self) -> Option<std::net::Ipv4Addr>;
+
+    fn subnet_mask(&self) -> Option<std::net::Ipv4Addr>;
+
+    fn gateway_addr(&self) -> Option<std::net::Ipv4Addr>;
+}
