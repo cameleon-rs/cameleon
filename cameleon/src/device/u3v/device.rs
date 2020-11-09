2786f6653ecacda0cfcc8b21fce43dded235854d
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2020-11-09 23:47:43 +0900

    Define u3v device

---
+++ cameleon/src/device/u3v/device.rs
@@ -0,0 +1,68 @@
+use cameleon_device::u3v;
+
+use super::control_handle::ControlHandle;
+
+use crate::device::{DeviceError, DeviceResult};
+
+pub type DeviceInfo = u3v::DeviceInfo;
+
+pub struct Device {
+    device: u3v::Device,
+
+    ctrl_handle: ControlHandle,
+    // TODO: Stream and event handles.
+}
+
+impl Device {
+    pub fn new(device: u3v::Device) -> DeviceResult<Self> {
+        let ctrl_handle = ControlHandle::new(&device)?;
+
+        Ok(Self {
+            device,
+            ctrl_handle,
+        })
+    }
+
+    pub fn open(&mut self) -> DeviceResult<()> {
+        if self.is_opened() {
+            return Ok(());
+        }
+
+        self.ctrl_handle.open()?;
+
+        Ok(())
+    }
+
+    pub fn control_handle(&self) -> DeviceResult<ControlHandle> {
+        if self.ctrl_handle.is_opened() {
+            Ok(self.ctrl_handle.clone())
+        } else {
+            Err(DeviceError::NotOpened)
+        }
+    }
+
+    pub fn is_opened(&self) -> bool {
+        self.ctrl_handle.is_opened()
+    }
+
+    pub fn close(&mut self) -> DeviceResult<()> {
+        self.ctrl_handle.close()
+    }
+
+    /// Basic information of the device. No need to call [`Device::open`] to obtain the
+    /// information.
+    pub fn device_info(&self) -> &DeviceInfo {
+        self.device.device_info()
+    }
+}
+
+/// Enumerate all devices connected to the host.
+pub fn enumerate_devices() -> DeviceResult<Vec<Device>> {
+    let devices = u3v::enumerate_devices()?;
+
+    Ok(devices
+        .into_iter()
+        .map(|dev| Device::new(dev))
+        .filter_map(|d| d.ok())
+        .collect())
+}
---
+++ cameleon/src/device/u3v/mod.rs
@@ -0,0 +1,31 @@
+mod control_handle;
+mod device;
+
+pub use control_handle::{ConnectionConfig, ControlHandle};
+pub use device::{enumerate_devices, Device};
+
+use cameleon_device::u3v;
+
+use super::DeviceError;
+
+impl From<u3v::Error> for DeviceError {
+    fn from(err: u3v::Error) -> DeviceError {
+        use u3v::Error::*;
+
+        match &err {
+            LibUsbError(libusb_error) => {
+                use u3v::LibUsbError::*;
+                match libusb_error {
+                    Io | InvalidParam | Access | Timeout | Overflow | Pipe | Interrupted
+                    | NoMem | NotSupported | BadDescriptor | Other => DeviceError::Io(err.into()),
+                    Busy => DeviceError::Busy,
+                    NoDevice | NotFound => DeviceError::Disconnected,
+                }
+            }
+
+            BufferIoError(_) | InvalidPacket(_) => DeviceError::Io(err.into()),
+
+            InvalidDevice => panic!("device is broken"),
+        }
+    }
+}
