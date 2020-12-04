2246d1dd3e73ded5b51e3e843aeda14737394cf5
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2020-12-04 14:34:05 +0900

    Declare GenTL Device module functions

--- cameleon-gentl/src/ffi/device.rs
+++ cameleon-gentl/src/ffi/device.rs
@@ -3,6 +3,8 @@ use std::{convert::TryInto, sync::Mutex};
 use super::*;
 
 pub(super) type DEV_HANDLE = *mut libc::c_void;
+pub(super) type PORT_HANDLE = *mut libc::c_void;
+pub(super) type DS_HANDLE = *mut libc::c_void;
 
 #[derive(Clone, Copy)]
 pub(super) struct DeviceModuleRef<'a> {
@@ -75,5 +77,63 @@ impl TryInto<imp::device::DeviceAccessFlag> for DEVICE_ACCESS_FLAGS {
             DEVICE_ACCESS_FLAGS::DEVICE_ACCESS_FLAGS_LIST_DEVICE_ACCESS_EXCLUSIVE => Ok(Exclusive),
             _ => Err(GenTlError::InvalidParameter),
         }
+    }
+}
+
+gentl_api! {
+    pub fn DevClose(hDevice: DEV_HANDLE) -> GenTlResult<()> {
+        todo!()
+    }
+}
+
+gentl_api! {
+    pub fn DevGetInfo(
+        hDevice: DEV_HANDLE,
+        iInfoCmd: DEVICE_INFO_CMD,
+        piType: *mut INFO_DATATYPE,
+        pBuffer: *mut libc::c_void,
+        piSize: *mut libc::size_t,
+    ) -> GenTlResult<()> {
+        todo!()
+    }
+}
+
+gentl_api! {
+    pub fn DevGetDataStreamID(
+        hDevice: DEV_HANDLE,
+        iIndex: u32,
+        sDataStreamID: *mut libc::c_char,
+        piSize: *mut libc::size_t,
+    ) -> GenTlResult<()> {
+        todo!()
+    }
+}
+
+gentl_api! {
+    pub fn DevGetNumDataStreams(hDevice: DEV_HANDLE, piNumDataStreams: *mut u32) -> GenTlResult<()>
+    {
+        todo!()
+    }
+}
+
+gentl_api! {
+    pub fn DevGetPort(hDevice: DEV_HANDLE, phRemoteDevice: *mut PORT_HANDLE) -> GenTlResult<()> {
+        todo!()
+    }
+}
+
+gentl_api! {
+    pub fn DevOpenDataStream(
+        hDevice: DEV_HANDLE,
+        sDataStreamID: *const ::std::os::raw::c_char,
+        phDataStream: *mut DS_HANDLE,
+    ) -> GenTlResult<()> {
+        todo!()
+    }
+}
+
+gentl_api! {
+    pub fn DevGetParentIF(hDevice: DEV_HANDLE, phIface: *mut interface::IF_HANDLE) -> GenTlResult<()> {
+        todo!()
     }
 }
