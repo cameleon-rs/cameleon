bfe5405eb239e7e2037ae248fcc9e91d2646cfc4
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-01-22 22:55:44 +0900

    Add doc for misc types of device

--- cameleon/src/device/u3v/mod.rs
+++ cameleon/src/device/u3v/mod.rs
@@ -1,3 +1,41 @@
+//! This module provides low level API for U3V compatible devices.
+//!
+//! # Examples
+//!
+//! ```no_run
+//! use cameleon::device::u3v;
+//! // Enumerate devices connected to the host.
+//! let mut devices = u3v::enumerate_devices().unwrap();
+//!
+//! // If no device is connected, return.
+//! if devices.is_empty() {
+//!     return;
+//! }
+//!
+//! let mut device = devices.pop().unwrap();
+//! // Open device.
+//! device.open().unwrap();
+//!
+//! // Get Abrm.
+//! let abrm = device.abrm().unwrap();
+//!
+//! // Read serial number from ABRM.
+//! let serial_number = abrm.serial_number().unwrap();
+//! println!("{}", serial_number);
+//!
+//! // Check user defined name feature is supported.
+//! // If it is suppoted, read from and write to the register.
+//! let device_capability = abrm.device_capability().unwrap();
+//! if device_capability.is_user_defined_name_supported() {
+//!     // Read from user defined name register.
+//!     let user_defined_name = abrm.user_defined_name().unwrap().unwrap();
+//!     println!("{}", user_defined_name);
+//!
+//!     // Write new name to the register.
+//!     abrm.set_user_defined_name("cameleon").unwrap();
+//! }
+//! ```
+
 pub mod register_map;
 
 mod control_handle;
