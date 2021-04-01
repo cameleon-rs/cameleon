e4d6c9273c78550641230f8391df1c200e66783e
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-04-01 22:55:36 +0900

    NodeStore adaptation in node

--- cameleon-genapi/src/parser/node.rs
+++ cameleon-genapi/src/parser/node.rs
@@ -1,6 +1,7 @@
 use super::{
     elem_name::{NODE, P_INVALIDATOR},
     node_base::{NodeAttributeBase, NodeBase, NodeElementBase},
+    node_store::{NodeId, NodeStore},
     xml, Parse,
 };
 
@@ -9,7 +10,7 @@ pub struct Node {
     attr_base: NodeAttributeBase,
     elem_base: NodeElementBase,
 
-    p_invalidators: Vec<String>,
+    p_invalidators: Vec<NodeId>,
 }
 
 impl Node {
@@ -19,19 +20,19 @@ impl Node {
     }
 
     #[must_use]
-    pub fn p_invalidators(&self) -> &[String] {
+    pub fn p_invalidators(&self) -> &[NodeId] {
         &self.p_invalidators
     }
 }
 
 impl Parse for Node {
-    fn parse(node: &mut xml::Node) -> Self {
+    fn parse(node: &mut xml::Node, store: &mut NodeStore) -> Self {
         debug_assert_eq!(node.tag_name(), NODE);
 
-        let attr_base = NodeAttributeBase::parse(node);
-        let elem_base = NodeElementBase::parse(node);
+        let attr_base = NodeAttributeBase::parse(node, store);
+        let elem_base = NodeElementBase::parse(node, store);
 
-        let p_invalidators = node.parse_while(P_INVALIDATOR);
+        let p_invalidators = node.parse_while(P_INVALIDATOR, store);
 
         Self {
             attr_base,
@@ -47,9 +48,10 @@ mod tests {
 
     use super::*;
 
-    fn node_from_str(xml: &str) -> Node {
+    fn node_from_str(xml: &str) -> (Node, NodeStore) {
+        let mut store = NodeStore::new();
         let document = xml::Document::from_str(xml).unwrap();
-        document.root_node().parse()
+        (document.root_node().parse(&mut store), store)
     }
 
     #[test]
@@ -77,35 +79,53 @@ mod tests {
             </Node>
             "#;
 
-        let node = node_from_str(&xml);
+        let (node, mut store) = node_from_str(&xml);
         let node_base = node.node_base();
-        assert_eq!(node_base.name(), "TestNode");
+        assert_eq!(node_base.id(), store.id_by_name("TestNode"));
         assert_eq!(node_base.name_space(), NameSpace::Standard);
         assert_eq!(node_base.merge_priority(), MergePriority::High);
         assert_eq!(node_base.expose_static().unwrap(), false);
 
         assert_eq!(node_base.tool_tip().unwrap(), "tooltip");
         assert_eq!(node_base.description().unwrap(), "the description");
-        assert_eq!(node_base.display_name(), "display name");
+        assert_eq!(node_base.display_name(), Some("display name"));
         assert_eq!(node_base.visibility(), Visibility::Guru);
         assert_eq!(node_base.docu_url().unwrap(), "http://FOO.com");
         assert_eq!(node_base.is_deprecated(), true);
         assert_eq!(node_base.event_id(), Some(0xF1));
-        assert_eq!(node_base.p_is_implemented().unwrap(), "AnotherNode0");
-        assert_eq!(node_base.p_is_available().unwrap(), "AnotherNode1");
-        assert_eq!(node_base.p_is_locked().unwrap(), "AnotherNode2");
-        assert_eq!(node_base.p_block_polling().unwrap(), "AnotherNode3");
+        assert_eq!(
+            node_base.p_is_implemented().unwrap(),
+            store.id_by_name("AnotherNode0")
+        );
+        assert_eq!(
+            node_base.p_is_available().unwrap(),
+            store.id_by_name("AnotherNode1")
+        );
+        assert_eq!(
+            node_base.p_is_locked().unwrap(),
+            store.id_by_name("AnotherNode2")
+        );
+        assert_eq!(
+            node_base.p_block_polling().unwrap(),
+            store.id_by_name("AnotherNode3")
+        );
         assert_eq!(node_base.imposed_access_mode(), AccessMode::RO);
         assert_eq!(node_base.p_errors().len(), 2);
-        assert_eq!(node_base.p_errors()[0], "AnotherErr0");
-        assert_eq!(node_base.p_errors()[1], "AnotherErr1");
-        assert_eq!(node_base.p_alias().unwrap(), "AnotherNode5");
-        assert_eq!(node_base.p_cast_alias().unwrap(), "AnotherNode6");
+        assert_eq!(node_base.p_errors()[0], store.id_by_name("AnotherErr0"));
+        assert_eq!(node_base.p_errors()[1], store.id_by_name("AnotherErr1"));
+        assert_eq!(
+            node_base.p_alias().unwrap(),
+            store.id_by_name("AnotherNode5")
+        );
+        assert_eq!(
+            node_base.p_cast_alias().unwrap(),
+            store.id_by_name("AnotherNode6")
+        );
 
         let p_invalidators = node.p_invalidators();
         assert_eq!(p_invalidators.len(), 2);
-        assert_eq!(p_invalidators[0].as_str(), "Invalidator0");
-        assert_eq!(p_invalidators[1].as_str(), "Invalidator1");
+        assert_eq!(p_invalidators[0], store.id_by_name("Invalidator0"));
+        assert_eq!(p_invalidators[1], store.id_by_name("Invalidator1"));
     }
 
     #[test]
@@ -115,15 +135,15 @@ mod tests {
             </Node>
             "#;
 
-        let node = node_from_str(&xml);
+        let (node, mut store) = node_from_str(&xml);
         let node_base = node.node_base();
-        assert_eq!(node_base.name(), "TestNode");
+        assert_eq!(node_base.id(), store.id_by_name("TestNode"));
         assert_eq!(node_base.name_space(), NameSpace::Custom);
         assert_eq!(node_base.merge_priority(), MergePriority::Mid);
         assert!(node_base.expose_static().is_none());
 
         assert!(node_base.tool_tip().is_none());
-        assert_eq!(node_base.display_name(), "TestNode");
+        assert_eq!(node_base.display_name(), None);
         assert_eq!(node_base.visibility(), Visibility::Beginner);
         assert!(node_base.docu_url().is_none());
         assert_eq!(node_base.is_deprecated(), false);
