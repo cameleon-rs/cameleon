3c370794843a7cd38e1d6d18263f802550ee495d
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-04-01 22:30:51 +0900

    NodeStore adaptation in float_reg

--- cameleon-genapi/src/parser/float_reg.rs
+++ cameleon-genapi/src/parser/float_reg.rs
@@ -2,6 +2,7 @@ use super::{
     elem_name::{DISPLAY_NOTATION, DISPLAY_PRECISION, ENDIANNESS, FLOAT_REG, REPRESENTATION, UNIT},
     elem_type::{register_node_elem, DisplayNotation, FloatRepresentation},
     node_base::{NodeAttributeBase, NodeBase},
+    node_store::NodeStore,
     register_base::RegisterBase,
     xml, Parse,
 };
@@ -57,17 +58,17 @@ impl FloatRegNode {
 }
 
 impl Parse for FloatRegNode {
-    fn parse(node: &mut xml::Node) -> Self {
+    fn parse(node: &mut xml::Node, store: &mut NodeStore) -> Self {
         debug_assert_eq!(node.tag_name(), FLOAT_REG);
 
-        let attr_base = node.parse();
-        let register_base = node.parse();
+        let attr_base = node.parse(store);
+        let register_base = node.parse(store);
 
-        let endianness = node.parse_if(ENDIANNESS).unwrap_or_default();
-        let unit = node.parse_if(UNIT);
-        let representation = node.parse_if(REPRESENTATION).unwrap_or_default();
-        let display_notation = node.parse_if(DISPLAY_NOTATION).unwrap_or_default();
-        let display_precision = node.parse_if(DISPLAY_PRECISION).unwrap_or(6);
+        let endianness = node.parse_if(ENDIANNESS, store).unwrap_or_default();
+        let unit = node.parse_if(UNIT, store);
+        let representation = node.parse_if(REPRESENTATION, store).unwrap_or_default();
+        let display_notation = node.parse_if(DISPLAY_NOTATION, store).unwrap_or_default();
+        let display_precision = node.parse_if(DISPLAY_PRECISION, store).unwrap_or(6);
 
         Self {
             attr_base,
@@ -100,7 +101,11 @@ mod tests {
         </FloatReg>
         "#;
 
-        let node: FloatRegNode = xml::Document::from_str(&xml).unwrap().root_node().parse();
+        let mut store = NodeStore::new();
+        let node: FloatRegNode = xml::Document::from_str(&xml)
+            .unwrap()
+            .root_node()
+            .parse(&mut store);
 
         assert_eq!(node.endianness(), register_node_elem::Endianness::BE);
         assert_eq!(node.unit().unwrap(), "Hz");
