19cb9bae02f54d70ad5d8d581a5fff299c182e5f
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-04-21 14:15:20 +0900

    Implement some utils methods to 'ValueStore'

--- genapi/src/store.rs
+++ genapi/src/store.rs
@@ -153,50 +153,147 @@ impl Default for DefaultNodeStore {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct ValueId(pub u32);
+pub struct ValueId(u32);
+
+impl ValueId {
+    pub fn from_u32(i: u32) -> Self {
+        Self(i)
+    }
+}
+
+macro_rules! declare_value_id {
+    ($name:ident) => {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        pub struct $name(u32);
+
+        impl Into<ValueId> for $name {
+            fn into(self) -> ValueId {
+                ValueId(self.0)
+            }
+        }
+
+        impl From<ValueId> for $name {
+            fn from(id: ValueId) -> Self {
+                Self(id.0)
+            }
+        }
+    };
+}
+
+declare_value_id!(IntegerId);
+declare_value_id!(FloatId);
+declare_value_id!(StringId);
+declare_value_id!(BooleanId);
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ValueData {
     Integer(i64),
     Float(f64),
     Str(String),
+    Boolean(bool),
 }
 
-impl ValueData {
-    /// # Panics
-    ///
-    /// This method panics if `self` is not `ValueData::Integer`.
-    #[must_use]
-    pub fn integer(&self) -> i64 {
-        if let Self::Integer(i) = self {
-            *i
+pub trait ValueStore {
+    fn store<T, U>(&mut self, data: T) -> U
+    where
+        T: Into<ValueData>,
+        U: From<ValueId>;
+
+    fn value_opt(&self, id: impl Into<ValueId>) -> Option<&ValueData>;
+
+    fn value_mut_opt(&mut self, id: impl Into<ValueId>) -> Option<&mut ValueData>;
+
+    fn value(&self, id: impl Into<ValueId>) -> &ValueData {
+        self.value_opt(id).unwrap()
+    }
+
+    fn value_mut(&mut self, id: impl Into<ValueId>) -> &mut ValueData {
+        self.value_mut_opt(id).unwrap()
+    }
+
+    fn integer_value(&self, id: IntegerId) -> Option<i64> {
+        if let ValueData::Integer(i) = self.value_opt(id)? {
+            Some(*i)
         } else {
-            panic!()
+            None
         }
     }
 
-    /// # Panics
-    ///
-    /// This method panics if `self` is not `ValueData::Float`.
-    #[must_use]
-    pub fn float(&self) -> f64 {
-        if let Self::Float(f) = self {
-            *f
+    fn integer_value_mut(&mut self, id: IntegerId) -> Option<&mut i64> {
+        if let ValueData::Integer(i) = self.value_mut_opt(id)? {
+            Some(i)
         } else {
-            panic!()
+            None
         }
     }
 
-    /// # Panics
-    ///
-    /// This method panics if `self` is not `ValueData::Str`.
-    #[must_use]
-    pub fn str(&self) -> &str {
-        if let Self::Str(s) = self {
-            s
+    fn float_value(&self, id: FloatId) -> Option<f64> {
+        if let ValueData::Float(f) = self.value_opt(id)? {
+            Some(*f)
         } else {
-            panic!()
+            None
         }
+    }
+
+    fn float_value_mut(&mut self, id: FloatId) -> Option<&mut f64> {
+        if let ValueData::Float(f) = self.value_mut_opt(id)? {
+            Some(f)
+        } else {
+            None
+        }
+    }
+
+    fn str_value(&self, id: StringId) -> Option<&str> {
+        if let ValueData::Str(s) = self.value_opt(id)? {
+            Some(s)
+        } else {
+            None
+        }
+    }
+
+    fn boolean_value(&self, id: BooleanId) -> Option<bool> {
+        if let ValueData::Boolean(b) = self.value_opt(id)? {
+            Some(*b)
+        } else {
+            None
+        }
+    }
+
+    fn boolean_value_mut(&mut self, id: BooleanId) -> Option<&mut bool> {
+        if let ValueData::Boolean(b) = self.value_mut_opt(id)? {
+            Some(b)
+        } else {
+            None
+        }
+    }
+
+    fn str_value_mut(&mut self, id: StringId) -> Option<&mut str> {
+        if let ValueData::Str(s) = self.value_mut_opt(id)? {
+            Some(s)
+        } else {
+            None
+        }
+    }
+}
+
+impl<T> ValueStore for &mut T
+where
+    T: ValueStore,
+{
+    fn store<U, V>(&mut self, data: U) -> V
+    where
+        U: Into<ValueData>,
+        V: From<ValueId>,
+    {
+        (*self).store(data)
+    }
+
+    fn value_opt(&self, id: impl Into<ValueId>) -> Option<&ValueData> {
+        (**self).value_opt(id)
+    }
+
+    fn value_mut_opt(&mut self, id: impl Into<ValueId>) -> Option<&mut ValueData> {
+        (*self).value_mut_opt(id)
     }
 }
 
@@ -218,48 +315,27 @@ impl From<String> for ValueData {
     }
 }
 
-pub trait ValueStore {
-    fn store(&mut self, data: impl Into<ValueData>) -> ValueId;
-
-    fn value(&self, id: ValueId) -> &ValueData;
-
-    fn value_mut(&mut self, id: ValueId) -> &mut ValueData;
-}
-
-impl<T> ValueStore for &mut T
-where
-    T: ValueStore,
-{
-    fn store(&mut self, data: impl Into<ValueData>) -> ValueId {
-        (*self).store(data)
-    }
-
-    fn value(&self, id: ValueId) -> &ValueData {
-        (**self).value(id)
-    }
-
-    fn value_mut(&mut self, id: ValueId) -> &mut ValueData {
-        (*self).value_mut(id)
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct DefaultValueStore(Vec<ValueData>);
 
 impl ValueStore for DefaultValueStore {
-    fn store(&mut self, data: impl Into<ValueData>) -> ValueId {
+    fn store<T, U>(&mut self, data: T) -> U
+    where
+        T: Into<ValueData>,
+        U: From<ValueId>,
+    {
         let id = u32::try_from(self.0.len())
             .expect("the number of value stored in `ValueStore` must not exceed u32::MAX");
         let id = ValueId(id);
         self.0.push(data.into());
-        id
+        id.into()
     }
 
-    fn value(&self, id: ValueId) -> &ValueData {
-        &self.0[id.0 as usize]
+    fn value_opt(&self, id: impl Into<ValueId>) -> Option<&ValueData> {
+        self.0.get(id.into().0 as usize)
     }
 
-    fn value_mut(&mut self, id: ValueId) -> &mut ValueData {
-        &mut self.0[id.0 as usize]
+    fn value_mut_opt(&mut self, id: impl Into<ValueId>) -> Option<&mut ValueData> {
+        self.0.get_mut(id.into().0 as usize)
     }
 }
