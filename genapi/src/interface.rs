47c981e29810236ce7d98f71e45facddb20dacad
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-04-28 10:23:47 +0900

    Define `IEnumeration`

--- genapi/src/interface.rs
+++ genapi/src/interface.rs
@@ -1,7 +1,7 @@
 use super::{
     elem_type::{DisplayNotation, FloatRepresentation, IntegerRepresentation},
-    store::{CacheStore, NodeStore, ValueStore},
-    {Context, Device, GenApiResult},
+    store::{CacheStore, NodeId, NodeStore, ValueStore},
+    EnumEntryNode, {Context, Device, GenApiResult},
 };
 
 #[derive(Clone, Debug)]
@@ -72,6 +72,8 @@ pub trait IInteger {
         store: impl NodeStore,
         cx: &mut Context<T, U>,
     ) -> GenApiResult<()>;
+
+    fn selected_by(&self, store: impl NodeStore) -> &[NodeId];
 }
 
 pub trait IFloat {
@@ -135,6 +137,57 @@ pub trait IFloat {
     fn set_max<T: ValueStore, U: CacheStore>(
         &self,
         value: f64,
+        device: impl Device,
+        store: impl NodeStore,
+        cx: &mut Context<T, U>,
+    ) -> GenApiResult<()>;
+}
+
+pub trait IString {
+    fn value<T: ValueStore, U: CacheStore>(
+        &self,
+        device: impl Device,
+        store: impl NodeStore,
+        cx: &mut Context<T, U>,
+    ) -> GenApiResult<String>;
+
+    fn set_value<T: ValueStore, U: CacheStore>(
+        &self,
+        value: &str,
+        device: impl Device,
+        store: impl NodeStore,
+        cx: &mut Context<T, U>,
+    ) -> GenApiResult<()>;
+
+    fn max_length<T: ValueStore, U: CacheStore>(
+        &self,
+        device: impl Device,
+        store: impl NodeStore,
+        cx: &mut Context<T, U>,
+    ) -> GenApiResult<usize>;
+}
+
+pub trait IEnumeration {
+    fn current_entry<T: ValueStore, U: CacheStore>(
+        &self,
+        device: impl Device,
+        store: impl NodeStore,
+        cx: &mut Context<T, U>,
+    ) -> GenApiResult<&EnumEntryNode>;
+
+    fn entries(&self, store: impl NodeStore) -> GenApiResult<&[EnumEntryNode]>;
+
+    fn set_entry_by_name<T: ValueStore, U: CacheStore>(
+        &self,
+        name: &str,
+        device: impl Device,
+        store: impl NodeStore,
+        cx: &mut Context<T, U>,
+    ) -> GenApiResult<()>;
+
+    fn set_entry_by_idx<T: ValueStore, U: CacheStore>(
+        &self,
+        idx: usize,
         device: impl Device,
         store: impl NodeStore,
         cx: &mut Context<T, U>,
