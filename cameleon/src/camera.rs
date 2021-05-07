d4fbabf88379abf46c1b7a6b4e5a1c8353a970e2
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-05-07 22:59:11 +0900

    Define `PayloadStream`

--- cameleon/src/camera.rs
+++ cameleon/src/camera.rs
@@ -1,5 +1,5 @@
-//! TODO: TBW
-use super::DeviceResult;
+//! This module contains types that is the main entry types of the `Cameleon`.
+use super::{payload::PayloadSender, DeviceResult, StreamResult};
 
 /// This trait provides I/O s to the device's memory.
 pub trait DeviceControl {
@@ -30,5 +30,11 @@ pub trait DeviceControl {
     fn disable_streaming(&mut self) -> DeviceResult<()>;
 }
 
-/// TODO:
-pub trait ImageStream {}
+/// This trait provides streaming capability.
+pub trait PayloadStream {
+    /// Start streaming.
+    fn run_streaming_loop(&mut self, sender: PayloadSender) -> StreamResult<()>;
+
+    /// Stop streaming.
+    fn stop_streaming_loop(&mut self) -> StreamResult<()>;
+}
