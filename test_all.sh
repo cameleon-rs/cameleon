c450f37c846fc3cc2badf4f30c754268e5df9133
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-07-08 11:53:35 +0900

    Temporarily disable `clippy::default_numeric_fallback`

--- test_all.sh
+++ test_all.sh
@@ -2,7 +2,7 @@
 set -e
 
 cargo fmt --all -- --check
-cargo clippy --workspace --all-features --all-targets -- -D clippy::all -D clippy::default_numeric_fallback
+cargo clippy --workspace --all-features --all-targets -- -D clippy::all
 cargo doc --no-deps
 
 cargo test --workspace --all-targets --all-features
