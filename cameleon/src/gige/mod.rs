51ac7a44b25e76f25a33e31741d9122eec86d24a
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2021-07-28 12:19:40 +0900

    Define error conversions

---
+++ cameleon/src/gige/mod.rs
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+pub mod control_handle;
+
+use cameleon_device::gige;
+
+use crate::ControlError;
+
+impl From<gige::Error> for ControlError {
+    fn from(err: gige::Error) -> Self {
+        match err {
+            gige::Error::Io(err) => ControlError::Io(err.into()),
+            gige::Error::InvalidPacket(msg) => ControlError::InvalidData(msg.into()),
+        }
+    }
+}
