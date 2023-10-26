2225292748760339294255d5b7a8b71501ad0217
Yoshitomo Nakanishi <yurayura.rounin.3@gmail.com>
2023-10-26 13:49:01 +0900

    Weaken lint-deny-level

--- test_all.sh
+++ test_all.sh
@@ -2,7 +2,7 @@
 set -e
 
 cargo fmt --all -- --check
-cargo clippy --workspace --all-features --all-targets -- -D clippy::all
+cargo clippy --workspace --all-features --all-targets -- -D warnings
 cargo doc --no-deps
 
 cargo test --workspace --all-targets --all-features
